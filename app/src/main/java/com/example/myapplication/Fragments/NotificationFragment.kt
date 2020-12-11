0cbd0fa48a7b8ed848eb227fd99bd205d76d694a
David Beradze <daviti.beradze.1@btu.edu.ge>
2020-12-11 22:38:16 +0400

    second commit

--- app/src/main/java/com/example/myapplication/Fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/NotificationFragment.kt
@@ -1,7 +1,22 @@
 package com.example.myapplication.Fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import android.widget.TextView
 import com.example.myapplication.R
 
 class NotificationFragment: Fragment(R.layout.fragment_notification) {
+
+    private lateinit var textView: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView = view.findViewById(R.id.textView2)
+
+        textView.text = NotificationFragmentArgs.fromBundle(requireArguments()).amount.toString()
+
+    }
+
 }
